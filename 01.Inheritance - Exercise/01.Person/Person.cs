30c3506817576ea315b2afff60272f9fbf88e08c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-26 11:38:16 +0300

    Initial commit

---
+++ 01.Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+
+            this.Name,
+
+            this.Age));
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
---
+++ 01.Inheritance - Exercise/02.Zoo/Reptile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Zoo
+{
+    public class Reptile : Animal
+    {
+        public Reptile(string name) : base(name)
+        {          
+        }      
+    }
+}
