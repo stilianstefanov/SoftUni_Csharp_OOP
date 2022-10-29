f770a9fc7990015058436aeef0d8d1e49912fad1
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-29 20:38:35 +0300

    Initial commit

---
+++ 02.Encapsulation - Lab/04.Team/Team.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get
+            {
+                return this.firstTeam.AsReadOnly();
+            }
+        }
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get
+            {
+                return this.reserveTeam.AsReadOnly();
+            }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {firstTeam.Count} players." + Environment.NewLine
+                + $"Second team has {reserveTeam.Count} players.";
+        }
+    }
+}
