d1f42b6b3251959afb6d089737c3fc1b07233a3f
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-31 19:07:44 +0200

    Update Box.cs

--- 02.Encapsulation - Exercise/01.ClassBoxData/Box.cs
+++ 02.Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -6,72 +6,62 @@ namespace ClassBoxData
 {
     public class Box
     {
-		private double length;
+        private double length;
         private double width;
         private double height;
 
-		public Box(double length, double width, double height)
-		{
-			Length = length;
-			Width = width;
-			Height = height;
-		}
-        public double Length
-		{
-			get { return length; }
-			private set 
-			{
-				if (value <= 0)
-				{
-					throw new ArgumentException($"Length cannot be zero or negative.");
-				}
-				length = value;
-			}
-		}
-		
-		public double Width
+        public Box(double length, double width, double height)
         {
-			get { return width; }
-			private set 
-			{
-				if (value <= 0)
-				{
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
                     throw new ArgumentException($"Width cannot be zero or negative.");
                 }
-				width = value; 
-			}
-		}
-		
-		public double Height
+                width = value;
+            }
+        }
+
+        public double Height
         {
-			get { return height; }
-			private set 
-			{
+            get { return height; }
+            private set
+            {
                 if (value <= 0)
                 {
                     throw new ArgumentException($"Height cannot be zero or negative.");
                 }
                 height = value;
-			}
-		}
-
-		public double SurfaceArea()
-		{
-            //2lw + 2lh + 2wh
-			return 2 * length * width + 2 * length * height + 2 * width * height;
+            }
         }
 
-		public double LateralSurfaceArea()
-		{
-			//2lh + 2wh
-			return 2 * length * height + 2 * width * height;
-        }
+        public double SurfaceArea()
+             => 2 * length * width + 2 * length * height + 2 * width * height;
 
-		public double Volume()
-		{
-            //lwh
-			return length * width * height;
-        }
+        public double LateralSurfaceArea()
+             => 2 * length * height + 2 * width * height;
 
+        public double Volume()
+             => length * width * height;
     }
 }
