b862b5356fd702187c941a9dd1c32bb04eaf820d
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-11-05 13:06:27 +0200

    Initial commit

---
+++ 04.Polymorphism - Lab/03.Shapes/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+       
+        public double Radius
+        {
+            get { return radius; }
+            private set { radius = value; }
+        }
+
+
+        public override double CalculateArea()
+            => Math.PI * (Radius * Radius);
+
+
+        public override double CalculatePerimeter()
+            => 2 * Math.PI * Radius;
+
+        public override string Draw()
+            => base.Draw() + this.GetType().Name;
+        
+    }
+}
---
+++ 04.Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+       
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public override double CalculateArea()
+            => this.Height * this.Width;
+
+
+        public override double CalculatePerimeter()
+            => 2 * this.Height + 2 * this.Width;
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+
+    }
+}
