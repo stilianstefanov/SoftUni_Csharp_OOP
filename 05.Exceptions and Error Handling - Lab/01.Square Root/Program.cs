72f51bb231d668cac83541b74f3525d64e62cfe3
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-11-09 12:58:19 +0200

    Initial commit

---
+++ 05.Exceptions and Error Handling - Lab/01.Square Root/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.Square_Root
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				int num = int.Parse(Console.ReadLine());
+				if (num < 0)
+					throw new ArgumentException("Invalid number.");
+
+				Console.WriteLine(Math.Sqrt(num));
+			}
+			catch (ArgumentException ae)
+			{
+
+				Console.WriteLine(ae.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Goodbye.");
+			}
+        }
+    }
+}
---
+++ 05.Exceptions and Error Handling - Lab/02.Enter Numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Enter_Numbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numCollection = new List<int>();
+
+            while (numCollection.Count < 10)
+            {
+                try
+                {
+                    if (!numCollection.Any())
+                    {
+                        numCollection.Add(ReadNumber(1, 100));
+                    }
+                    else
+                    {
+                        numCollection.Add(ReadNumber(numCollection.Max(), 100));
+                    }
+                }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", numCollection));
+        }
+
+        private static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+
+            int num;
+            
+            bool isParsed = int.TryParse(input, out num);
+            if (!isParsed)
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - {end}!");
+            }
+
+            return num;
+        }
+    }
+}
