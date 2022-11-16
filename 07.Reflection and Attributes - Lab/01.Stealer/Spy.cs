4d31e24f304510255361a70e99f164b462942bee
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-11-16 10:07:16 +0200

    Initial commit

---
+++ 07.Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type typeToInvestigate = Type.GetType(className);
+
+            object obj = Activator.CreateInstance(typeToInvestigate);
+
+            sb.AppendLine($"Class under investigation: {typeToInvestigate.FullName}");
+
+            FieldInfo[] fieldsInfo = typeToInvestigate.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+            foreach (FieldInfo field in fieldsInfo.Where(f => fields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(obj)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
