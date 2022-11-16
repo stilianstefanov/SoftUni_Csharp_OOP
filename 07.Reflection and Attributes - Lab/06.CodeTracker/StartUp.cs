8b7436689e6d3b2829383f132cfc588fdcff838c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-11-16 11:31:07 +0200

    Initial commit

---
+++ 07.Reflection and Attributes - Lab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+
+        
+    }
+}
---
+++ 07.Reflection and Attributes - Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
