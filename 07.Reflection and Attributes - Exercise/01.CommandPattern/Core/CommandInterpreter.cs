cd09da210916e112fa29da2cf2cd78d24c18770c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-11-16 14:02:41 +0200

    Initial commit

---
+++ 07.Reflection and Attributes - Exercise/01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,28 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Contracts;
+   
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] tokens = args.Split();
+
+            string commandName = tokens[0];
+            string[] value = tokens.Skip(1).ToArray();
+
+            Type commandType = Assembly.GetCallingAssembly().GetTypes()
+                .Where(t => t.Name.StartsWith(commandName))
+                .FirstOrDefault();
+
+            ICommand command = Activator.CreateInstance(commandType) as ICommand;
+
+
+            return command.Execute(value);
+        }
+    }
+}
---
+++ 07.Reflection and Attributes - Exercise/01.CommandPattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using Contracts;
+    using System;
+
+    internal class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            Environment.Exit(0);
+
+            return null;
+        }
+    }
+}
