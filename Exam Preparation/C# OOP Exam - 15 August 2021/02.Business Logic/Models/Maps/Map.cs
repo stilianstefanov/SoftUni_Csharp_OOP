a509bc44fce4d5f91474f5ae178857a2483925f3
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-11-29 15:38:27 +0200

    Update Map.cs

--- Exam Preparation/C# OOP Exam - 15 August 2021/02.Business Logic/Models/Maps/Map.cs
+++ Exam Preparation/C# OOP Exam - 15 August 2021/02.Business Logic/Models/Maps/Map.cs
@@ -2,29 +2,27 @@
 {
     using Racers.Contracts;
     using Utilities.Messages;
-    using Contracts;    
+    using Contracts;
 
     public class Map : IMap
-    {       
+    {
         public string StartRace(IRacer racerOne, IRacer racerTwo)
         {
             if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
-            {
                 return OutputMessages.RaceCannotBeCompleted;
-            }
 
             IRacer winningRacer = null;
-            IRacer loserRacer = null;            
+            IRacer loserRacer = null;
 
             if (racerOne.IsAvailable() && !racerTwo.IsAvailable())
             {
                 winningRacer = racerOne;
-                loserRacer = racerTwo;                
+                loserRacer = racerTwo;
             }
             else if (!racerOne.IsAvailable() && racerTwo.IsAvailable())
             {
                 winningRacer = racerTwo;
-                loserRacer = racerOne;                
+                loserRacer = racerOne;
             }
 
             if (winningRacer != null)
@@ -33,34 +31,24 @@
                 return string.Format(OutputMessages.OneRacerIsNotAvailable, winningRacer.Username, loserRacer.Username);
             }
 
-            double racerOneChances = racerOne.Car.HorsePower * racerOne.DrivingExperience * GetMultiplier(racerOne.RacingBehavior);
-            double racerTwoChances = racerTwo.Car.HorsePower * racerTwo.DrivingExperience * GetMultiplier(racerTwo.RacingBehavior);
+            double racerOneChances = GetWinningChances(racerOne);
+            double racerTwoChances = GetWinningChances(racerTwo);
 
             racerOne.Race();
             racerTwo.Race();
 
             if (racerOneChances > racerTwoChances)
-            {
                 winningRacer = racerOne;
-            }
             else
-            {
                 winningRacer = racerTwo;
-            }
-
+          
             return string.Format(OutputMessages.RacerWinsRace, racerOne.Username, racerTwo.Username, winningRacer.Username);
         }
 
+        private double GetWinningChances(IRacer racer) 
+            => racer.Car.HorsePower * racer.DrivingExperience * GetMultiplier(racer.RacingBehavior);
+
         private double GetMultiplier(string racingBehavior)
-        {
-            if (racingBehavior == "strict")
-            {
-                return 1.2;
-            }
-            else
-            {
-                return 1.1;
-            }
-        }
+            => racingBehavior == "strict" ? 1.2 : 1.1;       
     }
 }
