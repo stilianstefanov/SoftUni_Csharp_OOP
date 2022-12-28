42e972fe108e53504f6bb616ef806bf67259885a
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-12-28 12:52:06 +0200

    Initial commit

---
+++ DesignPatterns_Demos/Prototype/SandwichMenu.cs
@@ -0,0 +1,18 @@
+﻿namespace Prototype
+{
+    public class SandwichMenu
+    {
+        private Dictionary<string, SandwichPrototype> sandwiches;
+
+        public SandwichMenu()
+        {
+            sandwiches = new Dictionary<string, SandwichPrototype>();
+        }
+
+        public SandwichPrototype this[string name]
+        {
+            get => sandwiches[name];
+            set => sandwiches.Add(name, value);
+        }
+    }
+}
---
+++ DesignPatterns_Demos/Prototype/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace Prototype
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+            sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Turkey, Bacon", "American", "Lettuce, Tomato, Onion, Olives");
+            sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provoline", "Lettuce, Onion");
+            sandwichMenu["Vegitarian"] = new Sandwich("Wheat", "", "", "Lettuce, Onion, Tomato, Olives, Spinach");
+
+            //Clone
+            Sandwich sandwich1 = sandwichMenu["BLT"].Clone() as Sandwich;
+            Sandwich sandwich2 = sandwichMenu["ThreeMeatCombo"].Clone() as Sandwich;
+            Sandwich sandwich3 = sandwichMenu["Vegitarian"].Clone() as Sandwich;
+        }
+    }
+}
